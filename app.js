001c83e6f6ae022f72ac91270f0133760caf929d
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-07-27 00:10:59 +0100

    setting up the app

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+let a, b, c;
+let outputText;
+
+const validate = () => {
+    //get the input
+
+
+    //validate a, b, c
+
+
+
+    //calculate the result using: x= (-b+-sqrt(b^2-4ac))/2a
+
+    //output the result or errors
+}
